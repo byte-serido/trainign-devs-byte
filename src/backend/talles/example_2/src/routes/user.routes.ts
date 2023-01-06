79566aad7446929c92ed5d98c36e268afaf0ea6d
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-06 20:57:26 -0300

    new route, /auth

--- src/backend/talles/example_2/src/routes/user.routes.ts
+++ src/backend/talles/example_2/src/routes/user.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/users/userCase/createUser/CreateUserController";
 import { GetAllUserController } from "../modules/users/userCase/getAllUsers/GetAllUsersController";
+import { AuthUserController } from "../modules/users/userCase/authUser/AuthUserController";
 
 const createUserController = new CreateUserController();
 const getAllUsersController = new GetAllUserController();
+const authUserController = new AuthUserController();
 
 const userRoutes = Router();
 
+userRoutes.post("/auth", authUserController.handle);
 userRoutes.post("/create", createUserController.handle);
 userRoutes.get("/", getAllUsersController.handle);
 
