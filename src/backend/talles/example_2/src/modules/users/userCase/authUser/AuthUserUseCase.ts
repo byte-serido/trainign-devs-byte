7a48608f629f870b4bb7be0a2ab9a3e63d539263
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-09 18:06:12 -0300

    feat: user email and password validation

--- src/backend/talles/example_2/src/modules/users/userCase/authUser/AuthUserUseCase.ts
+++ src/backend/talles/example_2/src/modules/users/userCase/authUser/AuthUserUseCase.ts
@@ -1,28 +1,38 @@
-import { User, prisma } from "@prisma/client";
+import { User} from "@prisma/client";
+import { prisma } from "../../../../prisma/client";
 import { AuthUserDTO } from "../../dtos/AuthUserDTO";
 import { AppErros } from "../../../../errors/AppErros";
 import bcrypt from 'bcrypt';
 
 export class AuthUserUseCase {
-    async execute({email, password}: AuthUserDTO):Promise<User> {
+    async execute({email,password}: AuthUserDTO):Promise<User> {
         const userVerified = await prisma.user.findUnique({
 
             where: {
                 email,
-            },
-            select:{
-                password,
             },
         });
 
         if (!userVerified) {
             throw new AppErros("User with that email or password does not exist");
         }
+        
+        const userPassword = await prisma.user.findFirst({
+            
+            where: {
+                email,
+                password,
 
-            // if (!await bcrypt.compare(password, .password)) {
-            //     throw new AppErros("invalid Password");
-            // }
+        }});
 
-        throw new AppErros(" ", 200);
+
+        if (!userPassword) {
+            throw new AppErros("User with that email or password does not exist");
+        }
+
+        
+        
+        return userPassword;
+
+        }
     }
-}
