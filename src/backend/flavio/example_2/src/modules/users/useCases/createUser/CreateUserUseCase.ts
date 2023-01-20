928387e14ce9636350a226f0fa37208bafc80836
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-01-20 15:20:38 -0300

    mod createUser: controller e useCase

--- src/backend/flavio/example_2/src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/backend/flavio/example_2/src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,7 +1,7 @@
 import { User } from "@prisma/client";
 import { CreateUserDTO } from "../../dtos/CreateUserDTO";
 import { prisma } from "../../../../prisma/client";
-import { AppError } from "../../../../erros/AppError";
+//import { AppError } from "../../../../erros/AppError";
 
 import { bcrypt } from "bcrypt";
 
@@ -12,19 +12,7 @@ export class CreateUserUseCase {
     username,
     password,
   }: CreateUserDTO): Promise<User> {
-    //verificar se usuário já existe
-    const userAlreadyExists = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (userAlreadyExists) {
-      throw new AppError("User already exists!");
-    }
-
     //Criptografando a senha
-    //const bcrypt = require("bcrypt");
     const hash = await bcrypt.hash(password, 10);
     password = hash;
 
