f86ecb44391c6a5c9a9252da95c9f7a1b60644a9
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-01-24 12:36:16 -0300

    mod routes/index

--- src/backend/flavio/example_2/src/routes/index.ts
+++ src/backend/flavio/example_2/src/routes/index.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { userRoutes } from "./user.routes";
 import { movieRoutes } from "./movie.routes";
+import { projectRoutes } from "./project.routes";
+import { authRoutes } from "./auth.routes";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/movies", movieRoutes);
+
+routes.use("/auth", authRoutes);
+routes.use("/projects", projectRoutes);
 
 export { routes };
