903896b7521ed62b18e76d84121fd9545ba5415e
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-20 16:51:48 -0300

    :sparkles:feat: Adding NavBar Component and Global CSS #22

--- src/projects/forum_bsi/talles/forum_bsi/src/App.vue
+++ src/projects/forum_bsi/talles/forum_bsi/src/App.vue
@@ -1,13 +1,27 @@
 <template lang="">
   <div>
-    
-  </div>
+    <MyNavbar/>
+</div>
 </template>
 <script>
+import MyNavbar from './components/MyNavbar.vue';
 export default {
-  
+  components: {
+    MyNavbar,
+  },
 }
 </script>
-<style lang="">
-  
+<style>
+
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: rgb(89, 8, 89);
+  }
+
 </style>
