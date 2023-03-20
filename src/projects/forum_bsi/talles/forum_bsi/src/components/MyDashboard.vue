2deb4dbce3275a1dcf8950e1b0c5eac61be749e4
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-20 18:04:35 -0300

    :sparkles:feat: Initial sketch of the central dashboard, already styled #22

--- src/projects/forum_bsi/talles/forum_bsi/src/components/MyDashboard.vue
+++ src/projects/forum_bsi/talles/forum_bsi/src/components/MyDashboard.vue
@@ -1,19 +1,63 @@
 <template>
-    <div>
-        <h1>MyDashboard</h1>
+    <div id="Dash-main">
+        <div class="area-busca-dash">
+            <input type="text" placeholder="Escreva aqui sua duvida ou problema..." class="input-busca-dash" />
+            <img src="../../public/img/icons/icon-send.png" alt="send" id="send-input" />
+        </div>
+        <h1>Duvidas e Perguntas</h1>
     </div>
 </template>
 
 <script>
 export default {
-    name:"MyDashboard",
+    name: "MyDashboard",
     data() {
-        return {
-            
-        }
+        return {};
     },
-}
+};
 </script>
 <style scoped>
-    
+#Dash-main {
+    padding-left: 36px;
+    padding-top: 18px;
+    /* display: flex; */
+    /* background-color: rgb(107, 83, 227); */
+    width: 76.8%;
+    height: auto;
+}
+
+.area-busca-dash {
+    width: 98%;
+    height: 77px;
+    margin-bottom: 77px;
+}
+
+.input-busca-dash {
+    padding: 15px;
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    text-transform: lowercase;
+    background: #191622;
+}
+
+#send-input {
+    display: flex;
+    position: relative;
+    left: 96%;
+    bottom: 65%;
+}
+
+h1 {
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 24px;
+    color: #ff79c6;
+}
 </style>
