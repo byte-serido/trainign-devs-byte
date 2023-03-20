66de11542d6edb65e5312caf450f3a6b7b8b113a
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-20 17:07:52 -0300

    :construction:feat: Dashboard component

---
+++ src/projects/forum_bsi/talles/forum_bsi/src/components/MyDashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>MyDashboard</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"MyDashboard",
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style scoped>
+    
+</style>
