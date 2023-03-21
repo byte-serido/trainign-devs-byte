def63bcf6bfb45ff795cb9b50c019c001f8bcb87
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-21 19:54:56 -0300

    :sparkles:feat: Making style fixes and adding new members to the body (#22)

--- src/projects/forum_bsi/talles/forum_bsi/src/components/MyDashboard.vue
+++ src/projects/forum_bsi/talles/forum_bsi/src/components/MyDashboard.vue
@@ -1,6 +1,6 @@
 <template>
-<div id="Dashboard">
-    <div id="Dash-main">
+    <div id="Dashboard">
+        <div id="Dash-main">
             <div class="area-busca-dash">
                 <input type="text" placeholder="Escreva aqui sua duvida ou problema..." class="input-busca-dash" />
                 <img src="../../public/img/icons/icon-send.png" alt="send" id="send-input" />
@@ -12,29 +12,37 @@
                         {{ postagem.quest }}
                     </div>
                     <div class="interacoes">
-                        <img src="../../public/img/icons/icon-heart.svg" alt="" id="">
-                        <img src="../../public/img/icons/icon-thumbs-up.svg" alt="">
-                        <img src="../../public/img/icons/icon-thumbs-down.svg" alt="">
+                        <img src="../../public/img/icons/icon-heart.svg" alt="" id="" class="icons">
+                        <img src="../../public/img/icons/icon-thumbs-up.svg" alt="" class="icons">
+                        <img src="../../public/img/icons/icon-thumbs-down.svg" alt="" class="icons">
                     </div>
-            </div>
-            <div class="feed-vazio" v-if="!post">
-                <h3>
-                    Nenhuma pergunta disponivel ...T_T
-                </h3>
+                </div>
+                <div class="feed-vazio" v-if="!post">
+                    <h3>
+                        Nenhuma pergunta disponivel ...T_T
+                    </h3>
+                </div>
             </div>
         </div>
-    </div>
-    <div id="Dash-second">
-        <h1>Recentes</h1>
+        <div id="Dash-second">
+            <h1>Recentes</h1>
             <div id="posts-recentes">
                 <div class="new-post" v-show="post" v-for="postagem in posts" :key="postagem.id">
                     <div class="caixa-post">
                         {{ postagem.quest }}
                     </div>
+                    <div class="interacoes-second">
+                        <h3>Tallys</h3>
+                        <div style="background-color: transparent;">
+                            <img src="../../public/img/icons/icon-heart.svg" alt="" id="" class="icons">
+                            <img src="../../public/img/icons/icon-thumbs-up.svg" alt=""  class="icons">
+                            <img src="../../public/img/icons/icon-thumbs-down.svg" alt=""  class="icons">
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -44,9 +52,9 @@ export default {
         return {
             post: true,
             posts: [{
-                id: 1, quest: "Não consigo mudar a versão do meu Node no linux, como faço?" },
-                {id: 2, quest: "Não consigo mudar a versão do meu Vue no linux, como faço?" },
-                {id: 3, quest: "Não consigo mudar a versão do meu React no linux, como faço?" }
+                id: 1, quest: "Não consigo mudar a versão do meu Node no linux, como faço?" }
+                // {id: 2, quest: "Não consigo mudar a versão do meu Vue no linux, como faço?" },
+                // {id: 3, quest: "Não consigo mudar a versão do meu React no linux, como faço?" }
             ]
         };
     },
@@ -61,23 +69,23 @@ export default {
 }
 
 #Dash-main {
+    display: flex;
+    flex-direction: column;
     padding-left: 36px;
     padding-top: 18px;
-    /* display: flex; */
-    /* background-color: rgb(107, 83, 227); */
     width: 76%;
 }
 
 #Dash-second {
-    display: inline-block;
-    width: 24%;
-    min-height: 100%;
+    /* display: inline-block;
     position: relative;
-    left: 0%;
+    left: 0%; */
+    display: flex;
+    flex-direction: column;
+    width: 24%;
     border-left: 1px solid #292730;
     padding: 15px;
     background: #191622;
-
 }
 
 .area-busca-dash {
@@ -108,26 +116,22 @@ export default {
     bottom: 65%;
 }
 
-h1 {
-    font-weight: 700;
-    font-size: 22px;
-    line-height: 24px;
+h1,h2,h3 {
     color: #ff79c6;
+    background-color: transparent;
 }
 
 #section-post {
-
     max-width: 97%;
-    margin: 10px;
 }
 
 .postagens {
-    justify-content: space-between;
+    display: flex;
+    justify-content: space-around;
     align-items: center;
-    padding: 10px;
     width: 100%;
     min-height: 77px;
-    display: flex;
+    margin-top: 18px;
 }
 
 .post-quest {
@@ -147,9 +151,20 @@ h1 {
     background: #292730;
     display: flex;
     align-items: center;
+    padding: 5px;
 }
 
-.interacoes img {
+.interacoes-second {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 100%;
+    height: 62px;
+    background: #292730;
+    padding: 15px;
+}
+
+.icons {
     width: 22px;
     height: 20px;
     padding: 2px;
