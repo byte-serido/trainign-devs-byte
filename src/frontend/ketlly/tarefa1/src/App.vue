094a504cc90bfaefe19108752d281a30d43ed572
ketwy <ketllyazevedo14@gmail.com>
2023-03-23 09:43:58 -0300

    fix: adicionando classe aos inputs para estilização #24
    inicio da criação do corpo do projeto

--- src/frontend/ketlly/tarefa1/src/App.vue
+++ src/frontend/ketlly/tarefa1/src/App.vue
@@ -8,12 +8,17 @@
         </li>
       </ul>
     </nav>
-    <div id="pesquisa"><input type="text" placeholder="procure aqui ..."></div>
-    
+    <div id="pesquisa">
+      <input type="text" placeholder="procure aqui ..." class="fontTexto">
+    </div>
   </header>
-
   <main>
-
+    <div class="principal edit">
+      <div class="pergunta">
+        <input type="text" class="fontTexto" placeholder="escreva aqui sua duvida ou problema...">
+      </div>
+    </div>
+    <div class="lateral edit">adoleta</div>
   </main>
   <footer>
   </footer>
