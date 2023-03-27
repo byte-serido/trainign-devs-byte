02e7aafbc9af0dde5cac8a6da0de8cf4d1d965a0
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-03-27 19:47:32 -0300

    :lipstick: feat: CSS do MainForum.vue #23

--- src/projects/forum_bsi/flavio/forum_bsi/src/components/MainForum.vue
+++ src/projects/forum_bsi/flavio/forum_bsi/src/components/MainForum.vue
@@ -17,3 +17,30 @@ export default {
   name: "MainForum",
 };
 </script>
+
+<style>
+.main {
+  grid-area: main;
+  background: #13111b;
+  color: #ff79c6;
+}
+
+.boxcreate {
+  margin: 15px;
+  margin-left: 32px;
+  margin-right: 32px;
+  height: 40px;
+  border: 0;
+  border-radius: 4px;
+  background: #191622;
+  width: 80vh;
+  font-family: "Rubik", sans-serif;
+  font-size: 1rem;
+  color: #ffffff;
+  padding-left: 15px;
+}
+
+.boxcreate::placeholder {
+  color: #292730;
+}
+</style>
