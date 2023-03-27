879bf3fef0abe4466606a9b295305a91d221dfae
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-03-27 17:56:10 -0300

    :sparkles: feat: add NavbarForum.vue #23

--- src/projects/forum_bsi/flavio/forum_bsi/src/App.vue
+++ src/projects/forum_bsi/flavio/forum_bsi/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <NavbarForum />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarForum from "./components/NavbarForum.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavbarForum,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/projects/forum_bsi/flavio/forum_bsi/src/components/NavbarForum.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar">
+    <div class="profile">
+      <img class="img" :src="avatar" alt="avatar" height="40" width="40" />
+      <label>{{ nome }}</label>
+    </div>
+    <div>
+      <input
+        class="search"
+        type="search"
+        name=""
+        placeholder="Procure aqui"
+        id=""
+      />
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarForum",
+  data() {
+    return {
+      nome: "Lilia",
+      avatar: "/img/avatar.png",
+    };
+  },
+};
+</script>
