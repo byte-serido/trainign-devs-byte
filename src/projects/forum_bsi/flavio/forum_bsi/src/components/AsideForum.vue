be51013c7f1fab6af612c959c844d0af3fe6778f
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-03-27 19:45:48 -0300

    :lipstick: feat: CSS do AsideForum.vue #23

--- src/projects/forum_bsi/flavio/forum_bsi/src/components/AsideForum.vue
+++ src/projects/forum_bsi/flavio/forum_bsi/src/components/AsideForum.vue
@@ -13,3 +13,21 @@ export default {
   name: "AsideForum",
 };
 </script>
+<style>
+.sidebar {
+  grid-area: sidebar;
+  background: #191622;
+  color: #ff79c6;
+  padding: 17px;
+}
+
+.card p {
+  background: #13111b;
+  color: #ffffff;
+  width: 100%;
+  height: auto;
+  font-size: 1rem;
+  padding: 17px;
+  margin-top: 17px;
+}
+</style>
